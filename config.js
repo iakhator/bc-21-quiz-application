9f13a03f6c659ce1cb215e865695f6278a320c65
iakhator <iakhator>
2017-04-20 15:04:04 +0100

    remove duplicate config file

--- config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyAASntt54R-xeea7z_G2hp6qCQ_cH8sl4k",
-    authDomain: "quizapp-3676c.firebaseapp.com",
-    databaseURL: "https://quizapp-3676c.firebaseio.com",
-    projectId: "quizapp-3676c",
-    storageBucket: "quizapp-3676c.appspot.com",
-    messagingSenderId: "515269296181"
-  };
-  firebase.initializeApp(config);
